1ca01a0a86c2e46720b4c7b775519c03928139c7
deejayfakiofficial <71965798+deejayfakiofficial@users.noreply.github.com>
2021-09-27 15:22:23 +0400

    initial

--- acc/CartesianConverter.swift
+++ acc/CartesianConverter.swift
@@ -5,4 +5,27 @@
 //  Created by Stanislav Terentyev on 27.09.2021.
 //
 
-import Foundation
+import UIKit
+
+class CartesianConverter {
+    
+    let width: CGFloat
+    let halfWidth: CGFloat
+    let height: CGFloat
+    let halfHeight: CGFloat
+    
+    init (width: CGFloat, height: CGFloat) {
+        self.width = width
+        self.height = height
+        self.halfWidth = width / 2
+        self.halfHeight = height / 2
+    }
+    
+    func convertMiddleToTopLeft (_ point: CGPoint) -> CGPoint {
+        let x = point.x + halfWidth
+        let y = -point.y + halfHeight
+        
+        return CGPoint(x: x, y: y)
+    }
+    
+}
--- acc/CircleView.swift
+++ acc/CircleView.swift
@@ -5,4 +5,51 @@
 //  Created by Stanislav Terentyev on 27.09.2021.
 //
 
-import Foundation
+import UIKit
+
+class CircleView: UIView {
+
+    var circleColor: UIColor!
+    var middleCircleColor: UIColor!
+    var circleSize: CGFloat!
+    
+    init(size: CGFloat, color: UIColor, completeColor: UIColor) {
+        super.init(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        circleSize = size
+        circleColor = color
+        middleCircleColor = completeColor
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        self.layer.cornerRadius = circleSize / 2
+        self.layer.borderColor = UIColor.clear.cgColor
+        self.clipsToBounds = true
+        self.backgroundColor = circleColor
+        
+        frame.origin.x = UIScreen.main.bounds.width / 2 - circleSize / 2
+        frame.origin.y = UIScreen.main.bounds.height / 2 - circleSize / 2
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func move(to point: CGPoint, inMiddle: Bool = false) {
+        UIView.animate(withDuration: 0.1) {
+            self.frame.origin.x = point.x - self.circleSize / 2
+            self.frame.origin.y = point.y - self.circleSize / 2
+        }
+        if inMiddle && backgroundColor != middleCircleColor{
+            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn, animations: {
+                self.backgroundColor = self.middleCircleColor
+            }, completion: nil)
+        }
+        else if !inMiddle && backgroundColor != circleColor {
+            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn, animations: {
+                self.backgroundColor = self.circleColor
+            }, completion: nil)
+        }
+    }
+    
+}
+
+
